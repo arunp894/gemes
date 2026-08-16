72055751e5ad265e6b3ecec894c869d4560d86f0
arun1995-web <arunp894@gmail.com>
2026-08-16 08:24:57 +0530

    feat: bind carat weight input to the first row of line items

--- resources/views/purchases/_partials/_line_table.blade.php
+++ resources/views/purchases/_partials/_line_table.blade.php
@@ -101,7 +101,7 @@
                             </td>
                             <td>
                                 <input type="number" step="0.001" min="0" class="form-control form-control-sm"
-                                       v-model.number=""
+                                       v-model.number="line.rows[0].carat_weight"
                                        placeholder="ct">
                             </td>
                             <td>
